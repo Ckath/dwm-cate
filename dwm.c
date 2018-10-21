37e7f7032bc8dacc40262d1f22a3008b4ec90cfb
ckath <ckat@teknik.io>
2018-10-21 16:33:09 +0200

    split tile into ltile and rtile layouts, also multihead focus fixes

--- dwm.c
+++ dwm.c
@@ -219,7 +219,8 @@ static void sigchld(int unused);
 static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
-static void tile(Monitor *);
+static void ltile(Monitor *);
+static void rtile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void toggletag(const Arg *arg);
@@ -661,6 +662,8 @@ clientmessage(XEvent *e)
 	int i;
 
 	if (!c)
+		return;
+	if (c->mon != selmon)
 		return;
 	if (cme->message_type == netatom[NetWMState]) {
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
@@ -2006,7 +2009,7 @@ tagmon(const Arg *arg)
 }
 
 void
-tile(Monitor *m)
+ltile(Monitor *m)
 {
 	unsigned int i, n, h, smh, mw, my, ty;
 	Client *c;
@@ -2038,6 +2041,46 @@ tile(Monitor *m)
 			}
 			else
 				resize(c, m->wx + mw, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), False);
+			if(!(nexttiled(c->next)))
+				ty += HEIGHT(c) + smh;
+			else
+				ty += HEIGHT(c);
+		}
+}
+
+void
+rtile(Monitor *m)
+{
+	unsigned int i, n, h, smh, mw, my, ty;
+	Client *c;
+
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), n++);
+	if (n == 0)
+		return;
+
+	if (n > m->nmaster)
+		mw = m->nmaster ? m->ww * (1.0 - m->mfact) : 0;
+	else
+		mw = m->ww;
+	for (i = my = ty = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++)
+		if (i < m->nmaster) {
+			h = (m->wh - my) / (MIN(n, m->nmaster) - i);
+			resize(c, m->wx + m->ww - mw, m->wy + my, mw - (2*c->bw), h - (2*c->bw), 0);
+			my += HEIGHT(c);
+		} else {
+			smh = m->mh * m->smfact;
+			if(!(nexttiled(c->next)))
+				h = (m->wh - ty) / (n - i);
+			else
+				h = (m->wh - smh - ty) / (n - i);
+			if(h < minwsz) {
+				c->isfloating = True;
+				XRaiseWindow(dpy, c->win);
+				resize(c, m->mx + (m->mw / 2 - WIDTH(c) / 2), m->my + (m->mh / 2 - HEIGHT(c) / 2), m->ww - mw - (2*c->bw), h - (2*c->bw), False);
+				ty -= HEIGHT(c);
+			}
+			else
+				resize(c, m->wx, m->wy + ty, m->ww - mw - (2*c->bw), h - (2*c->bw), False);
 			if(!(nexttiled(c->next)))
 				ty += HEIGHT(c) + smh;
 			else
