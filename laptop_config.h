846b618eed8cbcc12368c9a7493d5101e82c552b
ckath <ckat@teknik.io>
2018-10-31 02:14:48 +0100

    change mfact to match the default ltile layout

--- laptop_config.h
+++ laptop_config.h
@@ -41,7 +41,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.45; /* factor of master area size [0.05..0.95] */
 static const float smfact    = 0.00; /* factor of tiled clients [0.00..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
